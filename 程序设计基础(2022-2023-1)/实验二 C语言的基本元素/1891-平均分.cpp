be1cb8afed5c590d460b65a9aeccdf703d480a8b
GongNanyue <50814347+GongNanyue@users.noreply.github.com>
2022-09-15 14:52:27 +0800

    Create 1891-平均分.cpp

---
+++ 程序设计基础(2022-2023-1)/实验二 C语言的基本元素/1891-平均分.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+const double Pi = 3.1415927;
+
+int main() {
+
+    cout.setf(ios::fixed);
+    cout.precision(1);
+    double a,b,c;
+    cin >> a >> b >> c;
+    cout << (a+b+c)/3<< endl;
+    return 0;
+}
