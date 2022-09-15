76662b78a3fa0e380da542e84f1040f3404abc11
GongNanyue <50814347+GongNanyue@users.noreply.github.com>
2022-09-15 15:04:41 +0800

    Create 2100-字符交换.cpp

---
+++ 程序设计基础(2022-2023-1)/作业二 C语言的基本元素/2100-字符交换.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+const double Pi = 3.1415926;
+
+int main() {
+    char a,b;
+    cin >> a >> b;
+    cout << b << " " << a << endl; 
+
+
+    return 0;
+}
