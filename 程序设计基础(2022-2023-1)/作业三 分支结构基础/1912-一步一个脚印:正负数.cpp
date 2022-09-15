f48dfea83bdcce22e2052a958dc031d67713570f
GongNanyue <50814347+GongNanyue@users.noreply.github.com>
2022-09-15 15:47:12 +0800

    Create 1912-一步一个脚印:正负数.cpp

---
+++ 程序设计基础(2022-2023-1)/作业三 分支结构基础/1912-一步一个脚印:正负数.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+int main() {
+    int x;
+    cin >> x;
+    if(x < 0){
+        cout << "-1\n";
+    } else if(x == 0){
+        cout << "0\n";
+    }else {
+        cout << "1\n";
+    }
+    return 0;
+}
