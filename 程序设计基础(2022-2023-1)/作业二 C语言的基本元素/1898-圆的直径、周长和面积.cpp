ef02fa7db351ec52f87c36b9265685c10c01f5e5
GongNanyue <50814347+GongNanyue@users.noreply.github.com>
2022-09-15 14:56:12 +0800

    Create 1898-圆的直径、周长和面积.cpp

---
+++ 程序设计基础(2022-2023-1)/作业二 C语言的基本元素/1898-圆的直径、周长和面积.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+const double Pi = 3.1415926;
+
+int main() {
+
+    cout.setf(ios::fixed);
+    cout.precision(2);
+    double r;
+    cin >> r;
+    cout << 2*r << " " << 2*r*Pi << " " << Pi*r*r << endl;
+    return 0;
+}
