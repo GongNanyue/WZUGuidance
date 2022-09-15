5d6d30da26065722c9852ee5b9220ca6a91a62aa
GongNanyue <50814347+GongNanyue@users.noreply.github.com>
2022-09-15 14:04:45 +0800

    Create 2098-输出图形III.cpp

---
+++ 程序设计基础(2022-2023-1)/作业一 C程序的开发环境/2098-输出图形III.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    cout <<"       *\n"
+           "      *\n"
+           "     *\n"
+           "*   *\n"
+           " * *\n"
+           "  *\n";
+    return 0;
+}
