6d28eb6dde30090a072f9025df0ea265aade470b
GongNanyue <50814347+GongNanyue@users.noreply.github.com>
2022-09-15 15:02:03 +0800

    Create 1900-温度转换.cpp

---
+++ 程序设计基础(2022-2023-1)/作业二 C语言的基本元素/1900-温度转换.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+const double Pi = 3.1415926;
+
+int main() {
+
+    cout.setf(ios::fixed);
+    cout.precision(1);
+    int f;
+    cin >> f;
+    cout << 5 / 9 * (f - 32)<< endl;
+
+
+    return 0;
+}
