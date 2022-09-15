e6d6565cccdf4e4b4fd1d05a9eb56eb4e019a963
GongNanyue <50814347+GongNanyue@users.noreply.github.com>
2022-09-15 14:59:52 +0800

    Create 1899-简单的四则运算.cpp

---
+++ 程序设计基础(2022-2023-1)/作业二 C语言的基本元素/1899-简单的四则运算.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+const double Pi = 3.1415926;
+
+int main() {
+
+    int a,b;
+    cin >> a >> b;
+    cout << a+b << " " << a*b << " " << a-b << " " << a/b  << endl;
+    return 0;
+}
