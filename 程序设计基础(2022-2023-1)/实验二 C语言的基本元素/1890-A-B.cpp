7f5d9b40eed579fcbc19266ef95409f78018735e
GongNanyue <50814347+GongNanyue@users.noreply.github.com>
2022-09-15 14:51:10 +0800

    Create 1890-A-B.cpp

---
+++ 程序设计基础(2022-2023-1)/实验二 C语言的基本元素/1890-A-B.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+const double Pi = 3.1415927;
+
+int main() {
+
+    cout.setf(ios::fixed);
+    cout.precision(2);
+    double a,b;
+    cin >> a >> b;
+    cout << a-b << endl;
+    return 0;
+}
