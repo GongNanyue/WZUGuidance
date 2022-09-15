6251d1f4d9fbb6bfa93d7048515a0c592ae59eb3
NekoMoYi <1254846416@qq.com>
2022-09-15 18:17:12 +0800

    1936-输出99乘法表

---
+++ 程序设计基础(2022-2023-1)/实验六 循环结构嵌套/1936-输出99乘法表.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<stdio.h>
+using namespace std;
+int main() {
+	int i,j;
+	for(i=1; i<=9; i++) {
+		for(j=1; j<=9; j++)
+			printf("%d*%d=%-3d", i,j,i*j);
+		cout<<endl;
+	}
+	return 0;
+}
