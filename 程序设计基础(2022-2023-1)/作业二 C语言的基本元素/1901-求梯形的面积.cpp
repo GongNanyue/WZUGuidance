a1747387224ba7f7342c6e7c26eae709f620eae6
GongNanyue <50814347+GongNanyue@users.noreply.github.com>
2022-09-15 15:03:31 +0800

    Create 1901-求梯形的面积.cpp

---
+++ 程序设计基础(2022-2023-1)/作业二 C语言的基本元素/1901-求梯形的面积.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+const double Pi = 3.1415926;
+
+int main() {
+
+    cout.setf(ios::fixed);
+    cout.precision(2);
+    double a,b,h;
+    cin  >> a >> b >> h;
+    cout << 0.5*(a+b)*h << endl;
+
+
+    return 0;
+}
