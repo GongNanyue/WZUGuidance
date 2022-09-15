8871171f0d903f8b982491d5fe160d95468d529b
GongNanyue <50814347+GongNanyue@users.noreply.github.com>
2022-09-15 14:49:32 +0800

    Create 1889-正方形.cpp

---
+++ 程序设计基础(2022-2023-1)/实验二 C语言的基本元素/1889-正方形.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+const double Pi = 3.1415927;
+
+int main() {
+
+    char a;
+    cin >> a;
+    cout << a << a << a << a << endl;
+    cout << a << "  " << a << endl;
+    cout << a << "  " << a << endl;
+    cout << a << a << a << a << endl;
+    return 0;
+}
