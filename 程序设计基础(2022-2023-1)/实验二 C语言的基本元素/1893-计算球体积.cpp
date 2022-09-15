54f8a661630345bc3116f2255ead66c3e2d0cbe9
GongNanyue <50814347+GongNanyue@users.noreply.github.com>
2022-09-15 14:41:57 +0800

    Create 1893-计算球体积.cpp

---
+++ 程序设计基础(2022-2023-1)/实验二 C语言的基本元素/1893-计算球体积.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+const double Pi = 3.1415927;
+
+int main() {
+
+    double r;
+    cin >> r;
+    cout.setf(ios::fixed);
+    cout.precision(3);
+    cout << 4.0/3.0*Pi*r*r*r << endl;
+    return 0;
+}
