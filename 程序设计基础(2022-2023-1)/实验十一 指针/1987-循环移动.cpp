6382274cd980f2dfb49281106f0276b7d8091495
GongNanyue <50814347+GongNanyue@users.noreply.github.com>
2022-09-17 22:53:47 +0800

    Update 1987-循环移动.cpp

--- 程序设计基础(2022-2023-1)/实验十一 指针/1987-循环移动.cpp
+++ 程序设计基础(2022-2023-1)/实验十一 指针/1987-循环移动.cpp
@@ -1,2 +1,26 @@
-//1987-循环移动
-//洗个澡先 ，等一下再写
+#include <bits/stdc++.h>
+
+using namespace std;
+int a[100];
+void myswap(int *p1, int *p2) {
+  int t = *p1;
+  *p1 = *p2;
+  *p2 = t;
+}
+int main() {
+  int n, m;
+  scanf("%d %d", &n, &m);
+  for (int i = 0; i < n; ++i) {
+    scanf("%d", &a[i]);
+  }
+  int *ip0 = a, *ip1 = a + n ;
+  for (; ip0 < a + n-m; ++ip0, ++ip1) {
+    myswap(ip0,ip1);
+  }
+  for(int *it = ip0;it != ip1;++it){
+    printf("%d",*it);
+    if(it != ip1-1)printf(" ");
+  }
+  printf("\n");
+  return 0;
+}
