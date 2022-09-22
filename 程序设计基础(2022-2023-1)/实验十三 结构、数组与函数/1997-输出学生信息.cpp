f887f17e56b77f600103dc4f97ff00af73c8c974
GongNanyue <50814347+GongNanyue@users.noreply.github.com>
2022-09-22 11:33:25 +0800

    Create 1997-输出学生信息.cpp

---
+++ 程序设计基础(2022-2023-1)/实验十三 结构、数组与函数/1997-输出学生信息.cpp
@@ -0,0 +1,111 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+const int subjectCnt = 3;
+
+typedef struct Student_ {
+  char id[10];
+  char name[100];
+  char sex;
+  int year;
+  int month;
+  int day;
+  int x;
+  int y;
+  int z;
+  double average;
+  double sum;
+} Student;
+
+void initStudent(Student *s);
+void insertStudent(Student *s, char *id, char *name, const char *sex,
+                   int year, int month, int day, int x, int y, int z);
+void countStudent(Student *s);
+void display(Student *s);
+void displayNew(Student *s);
+void listDisplay(Student *s, int n);
+Student ArrayStudent[100];
+
+int main() {
+  char op[10];
+  int idx = 0;
+  while (scanf("%s", op) && (op[0] != 'Q' && op[0] != 'E')) {
+    if(op[0]=='L'){
+      printf("List:\n");
+      listDisplay(ArrayStudent,idx);
+      continue;
+    }
+    Student student;
+    initStudent(&student);
+    scanf("%s %s %c %d %d %d %d %d %d",
+          student.id,
+          student.name,
+          &student.sex,
+          &student.year,
+          &student.month,
+          &student.day,
+          &student.x,
+          &student.y,
+          &student.z);
+    countStudent(&student);
+    ArrayStudent[idx++] = student;
+    displayNew(&student);
+  }
+  printf("Good bye!\n");
+  return 0;
+}
+
+void initStudent(Student *s) {
+  memset(s->id, '\0', sizeof s->id);
+  memset(s->name, '\0', sizeof s->name);
+  s->sex = '\0';
+  s->year = 0;
+  s->month = 0;
+  s->day = 0;
+  s->x = 0;
+  s->y = 0;
+  s->z = 0;
+  s->average = 0.0;
+  s->sum = 0.0;
+}
+
+void insertStudent(Student *s, char *id, char *name, const char *sex,
+                   int year, int month, int day, int x, int y, int z) {
+  strcpy(s->id, id);
+  strcpy(s->name, name);
+  s->sex = *sex;
+  s->year = year;
+  s->month = month;
+  s->day = day;
+  s->x = x;
+  s->y = y;
+  s->z = z;
+}
+void countStudent(Student *s) {
+  s->sum = s->x + s->y + s->z;
+  s->average = s->sum / subjectCnt;
+}
+void display(Student *s) {
+  printf("%s %s %c %d %d %d %.1lf %.1lf %.1lf %.1lf %.1lf\n", \
+
+         s->id,
+         s->name,
+         s->sex,
+         s->year,
+         s->month,
+         s->day,
+         (double) s->x,
+         (double) s->y,
+         (double) s->z,
+         s->average,
+         s->sum);
+}
+void displayNew(Student *s) {
+  printf("Insert:\n");
+  display(s);
+}
+void listDisplay(Student *s, int n) {
+  for (int i = 0; i < n; ++i) {
+    display(s + i);
+  }
+}
