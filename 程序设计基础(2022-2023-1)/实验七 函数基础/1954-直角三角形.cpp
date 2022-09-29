9b76a7b216e000b606f43bd342c970def8d7d7a4
GongNanyue <50814347+GongNanyue@users.noreply.github.com>
2022-09-29 11:05:24 +0800

    Update 1954-直角三角形.cpp

--- 程序设计基础(2022-2023-1)/实验七 函数基础/1954-直角三角形.cpp
+++ 程序设计基础(2022-2023-1)/实验七 函数基础/1954-直角三角形.cpp
@@ -1,18 +1,33 @@
 #include <bits/stdc++.h>
 
 using namespace std;
-
-bool is_tri(int c, int a, int b) {
-    return c * c == b * b + a * a;
+bool isTriangle(int a, int b, int c) {
+  if (a  + b  > c  || a + c > b  || b + c  > a ) {
+    return true;
+  } else {
+    return false;
+  }
+}
+bool isRightAngle(int c, int a, int b) {
+  return (c * c == b * b + a * a);
 }
 
 int main() {
-    int n,m;
-    cin >> n >> m;
-    int sum = 1;
-    for(int i = 0;i < m;++i){
-        sum *= n;
+  int T;
+  scanf("%d", &T);
+  for (int kase = 1; kase <= T; ++kase) {
+    int val;
+    scanf("%d", &val);
+    int sum = 0;
+    for (int i = 3; i < val; ++i) {
+      for (int j = i + 1; j < val; ++j) {
+        for (int k = j + 1; k <= val; ++k) {
+          if (isTriangle(k, i, j)&& isRightAngle(k,i,j))sum++;
+        }
+      }
     }
-    cout << sum << endl;
-    return 0;
+    printf("case#%d\n", kase);
+    printf("%d\n", sum);
+  }
+  return 0;
 }
