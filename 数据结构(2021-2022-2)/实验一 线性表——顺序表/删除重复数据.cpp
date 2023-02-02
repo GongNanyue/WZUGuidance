b0352e0bbe226f508a113bf5c091fd77a3976e4d
GongNanyue <50814347+GongNanyue@users.noreply.github.com>
2023-02-02 19:53:58 +0800

    Update 删除重复数据.cpp

--- 数据结构(2021-2022-2)/实验一 线性表——顺序表/删除重复数据.cpp
+++ 数据结构(2021-2022-2)/实验一 线性表——顺序表/删除重复数据.cpp
@@ -1,1 +1,17 @@
-#include <bits/stdc++.h>
+int myFind(SeqList *L, int pos, ElementType x) {
+  //pos是偏移量 0坐标开始 从>=pos开始查找
+  if (pos < 0 || pos >= L->length)return -1;
+  for (; pos < L->length; ++pos)
+    if (L->array[pos] == x)return pos;
+  return -1;
+}//返回偏移量
+
+void delRepeatElement(SeqList *L) {
+  for (int i = 0; i < L->length; ++i) {
+    int var = L->array[i], delPos = myFind(L, i + 1, var), buff;
+    while (delPos != -1) {
+      delElement(L, delPos + 1, &buff);
+      delPos = myFind(L, i + 1, var);
+    }
+  }
+}
