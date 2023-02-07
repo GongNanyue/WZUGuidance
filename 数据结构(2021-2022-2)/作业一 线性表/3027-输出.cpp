a3117d9cf645f4a341b0e6448d619c91b0c0e28d
GongNanyue <50814347+GongNanyue@users.noreply.github.com>
2023-02-07 18:59:56 +0800

    Create 3027-输出.cpp

---
+++ 数据结构(2021-2022-2)/作业一 线性表/3027-输出.cpp
@@ -0,0 +1,34 @@
+void printList(PolyList L) {
+  if (!L->next) {
+    puts("0");
+    return;
+  }
+  char res[100000], tmp[10000];
+  memset(res, 0, sizeof res);
+  for (PolyNode *np = L->next; np; np = np->next) {
+    memset(tmp, 0, sizeof tmp);
+    if (np->coef == 1) {
+      strcpy(tmp, "+");
+      if (np->exp == 0)
+        strcat(tmp, "1");
+    } else if (np->coef == -1) {
+      strcpy(tmp, "-");
+      if(np->exp == 0)
+        strcat(tmp,"1");
+    } else if (np->coef > 0)
+      sprintf(tmp, "+%d", np->coef);
+    else
+      sprintf(tmp, "%d", np->coef);
+    strcat(res, tmp);
+
+    memset(tmp, 0, sizeof tmp);
+    if (np->exp == 0) {}
+    else if (np->exp == 1)
+      sprintf(tmp, "x");
+    else
+      sprintf(tmp, "x^%d", np->exp);
+    strcat(res, tmp);
+  }
+  if (res[0] == '+' || res[0] == '-' && res[1] == '0') printf("%s", res + 1);
+  else printf("%s", res);
+}
