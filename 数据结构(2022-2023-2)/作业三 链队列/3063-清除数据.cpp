4acedc09c3202e9046660338c49b8721f54752a2
GongNanyue <50814347+GongNanyue@users.noreply.github.com>
2023-03-02 17:31:40 +0800

    Create 3063-清除数据.cpp

---
+++ 数据结构(2022-2023-2)/作业三 链队列/3063-清除数据.cpp
@@ -0,0 +1,11 @@
+void clearQueue(LinkQueue *Q) {
+  if (Q->front == NULL && Q->rear == NULL)return;
+  QNode *p0 = Q->front, *p1 = Q->front->next;
+  while (p1 != NULL) {
+    free(p0);
+    p0 = p1;
+    p1 = p1->next;
+  }
+  free(p0);
+  Q->front = Q->rear = NULL;
+}
