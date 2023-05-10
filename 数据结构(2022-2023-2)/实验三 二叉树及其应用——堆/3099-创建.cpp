ede91ad5603d374076c8bfd159929ad6b32824de
宫南月 <50814347+GongNanyue@users.noreply.github.com>
2023-05-10 17:55:36 +0800

    Update 3099-创建.cpp

--- 数据结构(2022-2023-2)/实验三 二叉树及其应用——堆/3099-创建.cpp
+++ 数据结构(2022-2023-2)/实验三 二叉树及其应用——堆/3099-创建.cpp
@@ -1,5 +1,6 @@
 Heap createHeap(int capacity) {
   Heap heap = (Heap) malloc(sizeof(HNode));
+  //第0个数据不使用
   heap->data = (ElementType *) malloc(sizeof(ElementType) * (capacity + 1));
   heap->size = 0;
   heap->capacity = capacity;
