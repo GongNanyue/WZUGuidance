9cfc8371008dfd2a82b361ab14f8dabccf6f4b73
宫南月 <50814347+GongNanyue@users.noreply.github.com>
2023-05-11 11:11:21 +0800

    Update 3109-初始化.cpp

--- 数据结构(2022-2023-2)/实验三 二叉树及其应用——堆/3109-初始化.cpp
+++ 数据结构(2022-2023-2)/实验三 二叉树及其应用——堆/3109-初始化.cpp
@@ -1,9 +1,10 @@
 void initHeap(Heap H, ElementType data[], int n) {
-  H->size = n;
-  for (int i = 0; i <= n; ++i)
+  //Heap H中的data 和 参数data都要堆化
+  H->size = n;//更改H的数据大小
+  for (int i = 0; i <= n; ++i)//i从1开始也可以
     H->data[i] = data[i];
   for (int i = n / 2; i >= 1; --i)
     heapify(H, i);
-  for (int i = 0; i <= n; ++i)
+  for (int i = 0; i <= n; ++i)//i从1开始也可以
     data[i] = H->data[i];
 }
