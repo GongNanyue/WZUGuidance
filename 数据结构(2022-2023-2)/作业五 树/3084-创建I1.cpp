7183790247f64d2de1952f2efa03a798c5c091a4
宫南月 <50814347+GongNanyue@users.noreply.github.com>
2023-05-11 09:32:48 +0800

    Create 3084-创建I1.cpp

---
+++ 数据结构(2022-2023-2)/作业五 树/3084-创建I1.cpp
@@ -0,0 +1,35 @@
+
+CSTree createTree(char s[]) {
+    CSTree root = NULL, node = NULL;
+    stack<CSTree> st;
+    int k = 0;
+
+    for (int i = 0; i < strlen(s); ++i) {
+        if (s[i] == '(') {
+            st.push(node);
+            k = 1;
+        } else if (s[i] == ',') {
+            k = 2;
+        } else if (s[i] == ')') {
+            st.pop();
+        } else {
+            node = (CSNode *) malloc(sizeof(CSNode));
+            node->data = s[i];
+            node->child = node->sibling = NULL;
+            if (root == NULL) {
+                root = node;
+            } else {
+                if (k == 1) {
+                    st.top()->child = node;
+                } else if (k == 2) {
+                    CSNode *chd = st.top()->child;
+                    while (chd->sibling != NULL) {
+                        chd = chd->sibling;
+                    }
+                    chd->sibling = node;
+                }
+            }
+        }
+    }
+    return root;
+}
