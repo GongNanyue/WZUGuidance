762497c27a92748ea7b3eae3045210c0d186c19e
宫南月 <50814347+GongNanyue@users.noreply.github.com>
2023-05-11 11:48:07 +0800

    Update 3115-保持子堆性质.cpp

--- 数据结构(2022-2023-2)/实验三 二叉树及其应用——哈夫曼树/3115-保持子堆性质.cpp
+++ 数据结构(2022-2023-2)/实验三 二叉树及其应用——哈夫曼树/3115-保持子堆性质.cpp
@@ -1,8 +1,10 @@
 void heapify(Heap H, int i) {
+  //也就是down()函数
   int smallest = i;
-  if (left(i) <= H->size && *H->data[left(i)] < *H->data[smallest])
+  if (left(i) <= H->size && *(H->data[left(i)]) < *(H->data[smallest]))
+    //注意比较的是HNode, 不是HNode*
     smallest = left(i);
-  if (right(i) <= H->size && *H->data[right(i)] < *H->data[smallest])
+  if (right(i) <= H->size && *(H->data[right(i)]) < *(H->data[smallest]))
     smallest = right(i);
   if (smallest != i) {
     swap(H, i, smallest);
