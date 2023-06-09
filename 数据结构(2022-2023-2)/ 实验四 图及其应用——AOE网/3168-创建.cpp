d90261cd97150ed1eeb26f2a88d20cf4977827ff
GongNanyue <50814347+GongNanyue@users.noreply.github.com>
2023-06-09 08:44:12 +0800

    Update 3168-创建.cpp

--- 数据结构(2022-2023-2)/ 实验四 图及其应用——AOE网/3168-创建.cpp
+++ 数据结构(2022-2023-2)/ 实验四 图及其应用——AOE网/3168-创建.cpp
@@ -1,5 +1,14 @@
-AOENetwork createGraph(){//  该函数创建一个空的AEO网并返回其指针。
 
-    AOENetworkStruct *aoe = new AOENetworkStruct;
-    return aoe;
+AOENetwork createGraph(){// 该函数创建一个空的AEO网并返回其指针。
+    AOENetwork G = (AOENetwork)malloc(sizeof(struct AOENetworkStruct));
+    G->edgeNum = 0;
+    G->vertexNum = 0;
+    for(int i = 0; i < MAXVEX; i++){
+        G->vexs[i].data = 0;
+        G->vexs[i].inDegree = 0;
+        G->vexs[i].earliest = 0;
+        G->vexs[i].latest = 0;
+        G->vexs[i].firstEdge = NULL;
+    }
+    return G;
 }
