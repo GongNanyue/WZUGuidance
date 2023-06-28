7faed9d1eb9cc12d0d6908eb7a4538634f0d9110
Blaine Murphy <50814347+GongNanyue@users.noreply.github.com>
2023-06-28 21:33:44 +0800

    Update 3172-插入边.cpp

--- 数据结构(2022-2023-2)/ 实验四 图及其应用——AOE网/3172-插入边.cpp
+++ 数据结构(2022-2023-2)/ 实验四 图及其应用——AOE网/3172-插入边.cpp
@@ -1,21 +1,20 @@
-
-void addEdge(AOENetwork g, VertexType v1, VertexType v2, int w) {
-    //该函数在AOE网g中增加一条从v1到v2权为w的边，如果已经有边则保留权最小的边。如果点v1或者v2不存在，则什么都不做。
-    int i = locateVertex(g, v1), j = locateVertex(g, v2);
-    if (i == -1 || j == -1)return;
-    ENode *eNode = findEdge(g, i, j);
-    if (eNode == NULL) {// 没有边
-        ENode *eNode1 = new ENode;
-        eNode1->weight = w;
-        //eNode1->latest = eNode1->earliest = -1;
-        eNode1->adjVertex = j;
-
-        eNode1->nextEdge = g->vexs[i].firstEdge;
-        g->vexs[i].firstEdge = eNode1;
-
-        g->edgeNum++;
-        ++g->vexs[j].inDegree;
-    } else {
-        eNode->weight = min(eNode->weight, w);
-    }
+void addEdge(AOENetwork g, VertexType v1, VertexType v2, int w){
+	int i = locateVertex(g,v1);
+	int j = locateVertex(g,v2);
+	
+	if(i != -1 && j != -1){
+		ENode *p = findEdge(g,i,j);
+		if(p != NULL){
+			p->weight = min(p->weight,w);	
+		}else{
+			p = new ENode;
+			p->weight = w;
+			p->adjVertex = j;
+			
+			p->nextEdge = g->vexs[i].firstEdge;
+			g->vexs[i].firstEdge = p;
+			g->edgeNum++;
+			g->vexs[j].inDegree++;
+		}
+	}
 }
