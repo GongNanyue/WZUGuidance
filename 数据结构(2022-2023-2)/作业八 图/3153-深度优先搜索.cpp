e545060dc3b3a82de7ba840844e30bbb5522d46b
Blaine Murphy <50814347+GongNanyue@users.noreply.github.com>
2023-06-29 10:37:01 +0800

    Update and rename 3153-深度优先搜索.cpp to 3153-深度优先搜索.md

--- 数据结构(2022-2023-2)/作业八 图/3153-深度优先搜索.cpp
+++
@@ -1,7 +0,0 @@
-void DFS(Graph g, int i){
-    g->vexs[i].visited = 1;
-    for(ENode *p = g->vexs[i].firstEdge;p != NULL;p = p->nextEdge){
-        if(!g->vexs[p->adjVertex].visited)
-            DFS(g,p->adjVertex);
-    }
-}
