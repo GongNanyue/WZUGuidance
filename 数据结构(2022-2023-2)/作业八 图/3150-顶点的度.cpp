26141de353596db29684aeedca6f84afaca9dc9a
Blaine Murphy <50814347+GongNanyue@users.noreply.github.com>
2023-06-29 08:47:38 +0800

    Update 3150-顶点的度.cpp

--- 数据结构(2022-2023-2)/作业八 图/3150-顶点的度.cpp
+++ 数据结构(2022-2023-2)/作业八 图/3150-顶点的度.cpp
@@ -1,7 +1,7 @@
 
 int degree(Graph g, VertexType v) {
     int i = locateVertex(g, v);
-    if (i == -1)return 0;
+    if (i == -1)return 0; // 可以不写 已经知道点一定在图中
 
     int cnt = 0;
     for (ENode *eNode = g->vexs[i].firstEdge; eNode != NULL; eNode = eNode->nextEdge) 
