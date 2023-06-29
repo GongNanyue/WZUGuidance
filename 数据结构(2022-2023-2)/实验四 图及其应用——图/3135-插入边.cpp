1da4bfb3ac33fb98b6120265af59787b8de1616a
Blaine Murphy <50814347+GongNanyue@users.noreply.github.com>
2023-06-29 08:07:08 +0800

    Update 3135-插入边.cpp

--- 数据结构(2022-2023-2)/实验四 图及其应用——图/3135-插入边.cpp
+++ 数据结构(2022-2023-2)/实验四 图及其应用——图/3135-插入边.cpp
@@ -1,6 +1,6 @@
 void addEdge(Graph g, VertexType x, VertexType y) {
   int xIdx = locateVertex(g, x), yIdx = locateVertex(g, y);
   if (xIdx == -1 || yIdx == -1 || g->edge[xIdx][yIdx])return;
-  g->edge[xIdx][yIdx] = g->edge[yIdx][xIdx] = 1; // 图用1代表有边 0代表没有边
+  g->edge[xIdx][yIdx] = g->edge[yIdx][xIdx] = 1; // 图用1代表有边 0代表没有边 图是无向图
   ++g->edgeNum; // 加上边数
 }
