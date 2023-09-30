a9308fbfa656e68324b3f6f8f7cf6d549816dc1c
GongNanyue <50814347+GongNanyue@users.noreply.github.com>
2023-09-30 09:28:15 +0800

    Create B. 两个数的乘积 ( The product of two numbers ).java

---
+++ 面向对象程序设计（Java）/实验一 基础/B. 两个数的乘积 ( The product of two numbers ).java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt(),b = scanner.nextInt();
+        System.out.println(a * b);
+        double c = scanner.nextDouble(),d = scanner.nextDouble();
+        System.out.println(c * d    );
+    }
+}
