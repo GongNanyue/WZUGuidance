56a73519cbdf1ffa797d1c1bf76314b42b14299f
GongNanyue <50814347+GongNanyue@users.noreply.github.com>
2024-03-15 14:23:18 +0800

    Update 1036 跟奥巴马一起编程.py

--- python应用开发/PTA/1036 跟奥巴马一起编程.py
+++ python应用开发/PTA/1036 跟奥巴马一起编程.py
@@ -1,9 +1,7 @@
 c = input().split()
 n = int(c[0])
 c = c[1]
-
-
 print(c * n)
-for i in range(n // 2 + n % 2 - 2):
+for i in range(int(n / 2 + 0.5) - 2):
     print(c + " " * (n - 2) + c)
 print(c * n)
