c7ff6d561d68afa4ffa814748ce88396ca70433c
Xyanxia <61773189+Xyanxia@users.noreply.github.com>
2024-11-15 16:18:34 +0800

    Create 0188-一步一个脚印：正负数II.cpp

---
+++ 程序设计基础(2022-2023-1)/基础练习/0188-一步一个脚印：正负数II.cpp
@@ -0,0 +1,23 @@
+#include <stdio.h>
+int main()
+{
+    int n,m;
+    scanf("%d",&n);
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%d",&m);
+        if (m>0)
+        {
+            printf("1\n");
+        }
+        else if (m<0)
+        {
+            printf("-1\n");
+        }
+        else
+        {
+            printf("0\n");
+        }
+    }
+    return 0;
+}
